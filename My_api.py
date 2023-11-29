f5bf4a234000d6507c4b742b683361b011b4abac
Romain Slomczynski <slomczynskiromain@yahoo.fr>
2023-11-29 21:24:27 +0300

    oui

--- My_api.py
+++ My_api.py
@@ -29,34 +29,34 @@ def hello():
     return "Hello My World"
 
 
-# @app.route("/credit/<id_client>", methods=['POST'])
-# # on créer une route qui va afficher la prédiction client (id_client étant utilisé dans la fonction ci-dessous)
-# #  Le client (streamlit) allant sur cette adresse  fait cette requête pour obtenir quelque chose (le return de la fonction).
+@app.route("/credit/<id_client>", methods=['POST'])
+# on créer une route qui va afficher la prédiction client (id_client étant utilisé dans la fonction ci-dessous)
+#  Le client (streamlit) allant sur cette adresse  fait cette requête pour obtenir quelque chose (le return de la fonction).
 
-# def credit(id_client):
+def credit(id_client):
 
-#     # récupération id client depuis argument url    
-#     # calcul prédiction défaut et probabilité de défaut
+    # récupération id client depuis argument url    
+    # calcul prédiction défaut et probabilité de défaut
 
-#     #  Conversion en integer de l'Id client
-#     ID = int(id_client)   
+    #  Conversion en integer de l'Id client
+    ID = int(id_client)   
     
-# # récupération des données clients
-#     X = dataframe[dataframe['SK_ID_CURR'] == ID]
-#     X = X.drop(['TARGET', 'SK_ID_CURR'], axis=1)
+# récupération des données clients
+    X = dataframe[dataframe['SK_ID_CURR'] == ID]
+    X = X.drop(['TARGET', 'SK_ID_CURR'], axis=1)
     
-#     prediction = model.predict(X)
+    prediction = model.predict(X)
     
-#     y_probabiliste = model.predict_proba(X)
+    y_probabiliste = model.predict_proba(X)
     
-#     dict_final = {
-#         'prediction' : int(prediction),
-#         'proba' : float(y_probabiliste[0][0])
-#         }
+    dict_final = {
+        'prediction' : int(prediction),
+        'proba' : float(y_probabiliste[0][0])
+        }
 
-#     print('Nouvelle Prédiction : \n', dict_final)
+    print('Nouvelle Prédiction : \n', dict_final)
 
-#     return jsonify(dict_final)
+    return jsonify(dict_final)
     
     
     
